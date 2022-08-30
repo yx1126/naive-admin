bfae3444a0e844f7bce83813593406ac50c4f05c
y <yx17714503091@163.com>
2022-08-30 17:32:33 +0800

    feat: add env

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 import routes from "./routes";
 
+const history = import.meta.env.MODE === "production" ? createWebHistory(import.meta.env.BASE_URL) : createWebHashHistory(import.meta.env.BASE_URL);
+
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history,
     routes: routes,
     scrollBehavior() {
         return {
