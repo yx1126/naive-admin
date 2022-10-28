c1f29dca86048989d5288b350c69419b566ee6be
y <yx17714503091@163.com>
2022-10-28 17:43:49 +0800

    feat: update css

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: `@import "@/style/global.scss";`,
+                additionalData: `@use "@/style/global.scss" as *;`,
             },
         },
     },
